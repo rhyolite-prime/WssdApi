d708fbe9d3982d39755b4367a8d48916f72564df
Emmanuel Addo Odame <eaodame@hubtel.com>
2026-09-25 23:48:33 +0000

    sapo integration

--- vendor/sapo/lib/cmake/SapoEngine/SapoEngineTargets-release.cmake
+++ vendor/sapo/lib/cmake/SapoEngine/SapoEngineTargets-release.cmake
@@ -5,15 +5,15 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "Sapo::sapo_core" for configuration "Release"
-set_property(TARGET Sapo::sapo_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(Sapo::sapo_core PROPERTIES
+# Import target "Sapo::core" for configuration "Release"
+set_property(TARGET Sapo::core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Sapo::core PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsapo_core.a"
   )
 
-list(APPEND _cmake_import_check_targets Sapo::sapo_core )
-list(APPEND _cmake_import_check_files_for_Sapo::sapo_core "${_IMPORT_PREFIX}/lib/libsapo_core.a" )
+list(APPEND _cmake_import_check_targets Sapo::core )
+list(APPEND _cmake_import_check_files_for_Sapo::core "${_IMPORT_PREFIX}/lib/libsapo_core.a" )
 
 # Import target "Sapo::sapoc" for configuration "Release"
 set_property(TARGET Sapo::sapoc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
